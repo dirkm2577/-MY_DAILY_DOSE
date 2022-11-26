3e3a82af80246a34e2f7d43ef863b25371a6f934
Dirk Mischke <dee2577@gmail.com>
2022-11-27 01:30:58 +0900

    cleared out old migration files

--- db/migrate/20221122191335_change_images_column_to_photos.rb
+++
@@ -1,7 +0,0 @@
-class ChangeImagesColumnToPhotos < ActiveRecord::Migration[7.0]
-  def change
-    change_table :supplements do |t|
-      t.rename :images, :photos
-    end
-  end
-end
--- db/migrate/20221126095835_drop_user_supplements_table.rb
+++
@@ -1,6 +0,0 @@
-class DropUserSupplementsTable < ActiveRecord::Migration[7.0]
-  def change
-    drop_table :user_supplements
-  end
-  add_reference :reviews, :user
-end
