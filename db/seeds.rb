7a6d274f24fecfe94baf59a19b838a10eab76ff4
Dirk Mischke <dee2577@gmail.com>
2022-11-29 18:23:16 +0900

    added 3 more seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,7 +15,7 @@ user1 = User.new(email: "alex@quest.com", password: "123456")
 
 puts "Creating Supplement 1"
 
-supplement1 = Supplement.new(name: "Vitamin D3", price: 12, description: "Das vegane Vitamin D3 von MRM ist eine einzigartige mikroverkapselte Cholecalciferol-Form von Vitamin D3, die ausschließlich aus Flechten extrahiert wird, um den Calciumbedarf und die Gesundheitsbedürfnisse der Knochen von Veganern und Vegetariern zu erfüllen.", user: user1, quantity: 60, frequency: 1, unit_measure: "125 mcg (5000 IU)", category: "vitamins")
+supplement1 = Supplement.new(name: "Vitamin D3", price: 12, description: "Vitamin D3 is the body's preferred form of vitamin D and can help support many aspects of health.Vitamin D3 (5,000 IU) is a high-potency vitamin D supplement and contains lanolin-derived cholecalciferol in soft fish gelatin capsules", user: user1, quantity: 60, frequency: 1, unit_measure: "125 mcg (5000 IU)", category: "vitamins")
 
 puts "Adding image 1"
 
@@ -70,3 +70,58 @@ puts "Adding image 9"
 file9 = URI.open('https://res.cloudinary.com/dach7c7ct/image/upload/v1669217655/resveratrol_image_3_kuknfy.png')
 supplement3.images.attach(io: file9, filename: 'resveratrol_image_3.png', content_type: 'image/png')
 supplement3.save!
+
+puts "Creating Supplement 4"
+
+supplement1 = Supplement.new(name: "Vitamin C", price: 9, description: "Dietary supplements in gummies form can be a great and tasty way to add essential nutrients to your daily routine, especially if you don't like taking capsules or tablets.", user: user1, quantity: 90, frequency: 3, unit_measure: "250 mg", category: "vitamins")
+
+puts "Adding image 10"
+
+file10 = URI.open('https://res.cloudinary.com/dach7c7ct/image/upload/v1669711075/Vitamin_C_image_1_vjlipy.png')
+supplement1.images.attach(io: file10, filename: 'vitamin_c_image_1.png', content_type: 'image/png')
+supplement1.save!
+
+puts "Adding image 11"
+file11 = URI.open('https://res.cloudinary.com/dach7c7ct/image/upload/v1669711077/Vitamin_C_image_2_eip0r0.png')
+supplement1.images.attach(io: file11, filename: 'vitamin_c_image_2.png', content_type: 'image/png')
+supplement1.save!
+
+puts "Adding image 12"
+file12 = URI.open('https://res.cloudinary.com/dach7c7ct/image/upload/v1669711077/Vitamin_C_image_3_eoobbm.png')
+supplement1.images.attach(io: file12, filename: 'vitamin_c_image_3.png', content_type: 'image/png')
+supplement1.save!
+
+puts "Creating Supplement 5"
+
+supplement1 = Supplement.new(name: "5-HTP", price: 10, description: "5-HTP is extracted from the seeds of the African black bean. In the brain, 5-HTP is converted into serotonin, a neurotransmitter found at the junctions (synapses) between neurons. It has a calming and relaxing effect and supports mental and emotional well-being as well as healthy sleep.", user: user1, quantity: 60, frequency: 1, unit_measure: "100 mg", category: "weight-loss")
+
+puts "Adding image 13"
+
+file13 = URI.open('https://res.cloudinary.com/dach7c7ct/image/upload/v1669712493/5-HTP_image_1_mwibwn.png')
+supplement1.images.attach(io: file13, filename: '5-HTP_image_1.png', content_type: 'image/png')
+supplement1.save!
+
+puts "Adding image 14"
+file14 = URI.open('https://res.cloudinary.com/dach7c7ct/image/upload/v1669712493/5-HTP_image_2_pmizgm.png')
+supplement1.images.attach(io: file14, filename: '5-HTP_image_2.png', content_type: 'image/png')
+supplement1.save!
+
+puts "Adding image 15"
+file15 = URI.open('https://res.cloudinary.com/dach7c7ct/image/upload/v1669712507/5-HTP_image_3_cpogwe.png')
+supplement1.images.attach(io: file15, filename: '5-HTP_image_3.png', content_type: 'image/png')
+supplement1.save!
+
+puts "Creating Supplement 6"
+
+supplement1 = Supplement.new(name: "High Absorption Magnesium", price: 12, description: "Doctor's Best Magnesium contains a daily dose of magnesium with high absorption capacity without burdening the gastrointestinal tract. This superior formula with 100% chelated lysinate glycinate magnesium absorbs effectively to support muscle relaxation and optimal nerve function.", user: user1, quantity: 60, frequency: 2, unit_measure: "200 mg", category: "minerals")
+
+puts "Adding image 16"
+
+file16 = URI.open('https://res.cloudinary.com/dach7c7ct/image/upload/v1669713211/Magnesium_image_1_htsnig.png')
+supplement1.images.attach(io: file16, filename: 'Magnesium_image_1.png', content_type: 'image/png')
+supplement1.save!
+
+puts "Adding image 17"
+file17 = URI.open('https://res.cloudinary.com/dach7c7ct/image/upload/v1669713211/Magnesium_image_2_jupgod.png')
+supplement1.images.attach(io: file17, filename: 'Magnesium_image_2.png', content_type: 'image/png')
+supplement1.save!
