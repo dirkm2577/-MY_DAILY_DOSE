717b2486085474718cb3c70407d3e5aaf3e7e3c8
Dirk Mischke <dee2577@gmail.com>
2022-11-30 03:59:03 +0900

    webpacker
    Signed-off-by: Dirk Mischke <dee2577@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,11 @@
 const path    = require("path")
 const webpack = require("webpack")
-
+const { environment } = require("@rails/webpacker")
+environment.config.delete('node.dgram')
+environment.config.delete('node.fs')
+environment.config.delete('node.net')
+environment.config.delete('node.tls')
+environment.config.delete('node.child_process')
 module.exports = {
   mode: "production",
   devtool: "source-map",
@@ -11,6 +16,15 @@ module.exports = {
     filename: "[name].js",
     sourceMapFilename: "[file].map",
     path: path.resolve(__dirname, "app/assets/builds"),
+  },
+  resolve: {
+    fallback: {
+      dgram: false,
+      fs: false,
+      net: false,
+      tls: false,
+      child_process: false
+    }
   },
   plugins: [
     new webpack.optimize.LimitChunkCountPlugin({
