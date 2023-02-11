3d0b6cce7a64ebcaa584ceeaa14525910728a0e0
Dirk Mischke <dee2577@gmail.com>
2023-02-11 09:06:24 +0100

    added geocode gem and added address and coordinates to user schema

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,13 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  geocoded_by :address
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   has_many :supplements, dependent: :destroy
   has_many :reviews
   has_many :notifications, as: :recipient, dependent: :destroy
+
+  after_validation :geocode, if: :will_save_change_to_address?
 end
