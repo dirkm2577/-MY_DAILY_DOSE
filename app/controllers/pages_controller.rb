25443f821c671cbff3720b50d7602e7ca29fa9de
Dirk Mischke <dee2577@gmail.com>
2023-02-11 12:29:09 +0100

    added map and marker functionality - not finished - need less precise address info to work

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,12 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    @users = User.all
+    # The `geocoded` scope filters only users with coordinates
+    @markers = @users.geocoded.map do |user| {
+        lat: user.latitude,
+        lng: user.longitude
+      }
+    end
   end
 end
---
+++ app/javascript/controllers/map_controller.js
@@ -0,0 +1,42 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="map"
+export default class extends Controller {
+  static values = {
+    apiKey: String,
+    markers: Array
+  }
+
+  connect() {
+    mapboxgl.accessToken = 'pk.eyJ1IjoiZC1taXNjaGtlIiwiYSI6ImNsOGRhYTh4OTB0YjkzdW1yOGFzNGU1cjUifQ.DdhoD-Xf2Qhm3CIO6PdCKw';
+    const map = new mapboxgl.Map({
+      container: 'map', // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: [51, 6], // starting position [lng, lat]
+      zoom: 9, // starting zoom
+    });
+    mapboxgl.accessToken = this.apiKeyValue
+
+    this.map = new mapboxgl.Map({
+      container: this.element,
+      style: "mapbox://styles/mapbox/streets-v10"
+    })
+
+    this.#addMarkersToMap()
+    this.#fitMapToMarkers()
+  }
+
+  #addMarkersToMap() {
+    this.markersValue.forEach((marker) => {
+      new mapboxgl.Marker()
+        .setLngLat([marker.lng, marker.lat])
+        .addTo(this.map)
+    })
+  }
+
+  #fitMapToMarkers() {
+    const bounds = new mapboxgl.LngLatBounds()
+    this.markersValue.forEach(marker => bounds.extend([marker.lng, marker.lat]))
+    this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
+  }
+}
